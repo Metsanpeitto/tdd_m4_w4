dbf36c8ae0e474ffea7f2cc8af73511b55d8fdbe
Metsanpeitto <abraham@waldenberginc.com>
2021-10-14 15:45:41 +0200

    Fix rubocop

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 class Solver
-  def factorial(n)
-    if n > -1
-      (1..n).inject(:*) || 1
+  def factorial(num)
+    if num > -1
+      (1..num).inject(:*) || 1
     else
       'Only numbers larger than 0 please'
     end
@@ -11,11 +11,11 @@ class Solver
     word.reverse
   end
 
-  def fizzbuzz(n)
-    response = n.to_s
-    puts (n % 3).zero?
-    response = 'fizz' if (n % 3).zero?
-    if (n % 5).zero?
+  def fizzbuzz(num)
+    response = num.to_s
+    puts (num % 3).zero?
+    response = 'fizz' if (num % 3).zero?
+    if (num % 5).zero?
       if response == 'fizz'
         response += 'buzz'
       else
